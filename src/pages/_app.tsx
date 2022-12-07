5431c3425b20548a2e336a2d83f427154b56c41b
y-solb <ysolb99@gmail.com>
2022-12-07 18:09:41 +0900

    Revert "Feat: next-auth 설치 및 카카오로그인 1차 개발 중"
    This reverts commit b2c7236c74a5799c5cdc0f4fbf39f1f0c648d118.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import Head from 'next/head';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
 import Script from 'next/script';
-
-import { SessionProvider } from 'next-auth/react';
 
 declare global {
   interface Window {
@@ -15,36 +12,11 @@ declare global {
 function App({ Component, pageProps }: AppProps) {
   function kakaoInit() {
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-    console.log(window.Kakao.isInitialized());
   }
 
   return (
     <>
-      <Head>
-        <title>스타일씨 [STYLE C]</title>
-        <meta name="description" content="스타일씨 [STYLE C] 공식 스토어" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/imgs/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/imgs/favicon/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/imgs/favicon/favicon-16x16.png"
-        />
-      </Head>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Component {...pageProps} />
       <Script
         src="https://developers.kakao.com/sdk/js/kakao.js"
         onLoad={kakaoInit}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,51 @@
+import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import React from 'react';
-import Link from 'next/link';
 
 export default function Home() {
+  const loginFormWithKakao = () => {
+    window.Kakao.Auth.authorize({
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+    });
+  };
+
+  const loginFormWithKakao2 = () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+  };
+
   return (
     <div className={styles.container}>
+      <Head>
+        <title>스타일씨 [STYLE C]</title>
+        <meta name="description" content="스타일씨 [STYLE C] 공식 스토어" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/imgs/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/imgs/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/imgs/favicon/favicon-16x16.png"
+        />
+      </Head>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to STYLEC</h1>
-        <div style={{ fontSize: '2em', marginTop: '10px' }}>
-          <Link href="/login">login</Link>
-        </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <h2>javascript 방식</h2>
+        <button onClick={loginFormWithKakao}>카카오 로그인</button>
+        <h2>rest api 방식</h2>
+        <button onClick={loginFormWithKakao2}>카카오 로그인</button>
       </main>
     </div>
   );
